51c535f3b2f3a42c63b07733f426e6cfc3ba7695
John Goering <johngoering@me.com>
2022-12-01 13:12:35 +0100

    solutions for day 1

--- AdventOfCode2022/Solutions/Puzzle1-1.swift
+++ AdventOfCode2022/Solutions/Puzzle1-1.swift
@@ -10,9 +10,33 @@ struct Puzzle11: View {
     
     var body: some View {
         PuzzleView(code: """
-    return "computed solution"
+                // split into inventories by blank lines (2 line breaks)
+                let elfInventories = data11.split(separator: "\\\\n\\\\n")
+                let inventorySums = elfInventories.map {
+                    inventory in
+                    // for each inventory -> for each line -> add up the entries
+                    inventory
+                        .split(separator: "\\n")
+                        .reduce(0) { singleEntry, str in
+                            singleEntry + Int(str)!
+                    }
+                }
+                // return the the largest amount of total calories
+                return "\\(inventorySums.max()!)"
     """) {
-            return "computed solution"
+            // split into inventories by blank lines (2 line breaks)
+            let elfInventories = data11.split(separator: "\n\n")
+            let inventorySums = elfInventories.map {
+                inventory in
+                // for each inventory -> for each line -> add up the entries
+                inventory
+                    .split(separator: "\n")
+                    .reduce(0) { singleEntry, str in
+                        singleEntry + Int(str)!
+                }
+            }
+            // return the the largest amount of total calories
+            return "\(inventorySums.max()!)"
         }
     }
 }
--- AdventOfCode2022/Solutions/Puzzle1-2.swift
+++ AdventOfCode2022/Solutions/Puzzle1-2.swift
@@ -10,9 +10,37 @@ struct Puzzle12: View {
     
     var body: some View {
         PuzzleView(code: """
-    return "computed solution"
+            // split into inventories by blank lines (2 line breaks)
+            let elfInventories = data11.split(separator: "\\n\\n")
+            let inventorySums = elfInventories.map {
+                inventory in
+                // for each inventory -> for each line -> add up the entries
+                inventory
+                    .split(separator: "\\n")
+                    .reduce(0) { singleEntry, str in
+                        singleEntry + Int(str)!
+                }
+            }
+            var sorted:[Int] = inventorySums.sorted().reversed()
+            sorted = [sorted[0],sorted[1],sorted[2]]
+            // return the the largest amount of total calories
+            return "\\(sorted.reduce(0, +))"
     """) {
-            return "computed solution"
+            // split into inventories by blank lines (2 line breaks)
+            let elfInventories = data11.split(separator: "\n\n")
+            let inventorySums = elfInventories.map {
+                inventory in
+                // for each inventory -> for each line -> add up the entries
+                inventory
+                    .split(separator: "\n")
+                    .reduce(0) { singleEntry, str in
+                        singleEntry + Int(str)!
+                }
+            }
+            var sorted:[Int] = inventorySums.sorted().reversed()
+            sorted = [sorted[0],sorted[1],sorted[2]]
+            // return the the largest amount of total calories
+            return "\(sorted.reduce(0, +))"
         }
     }
 }
