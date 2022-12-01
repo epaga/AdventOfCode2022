c970468330a86f90bf4f9b5467146f69312d7ed7
John Goering <johngoering@me.com>
2022-12-01 12:57:13 +0100

    Initial Commit

---
+++ AdventOfCode2022/AdventOfCode2022App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2022App.swift
+//  AdventOfCode2022
+//
+//  Created by John Goering on 01.12.22.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
